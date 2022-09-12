88289fd622b8fa2c36b8940c3012454271206c0e
ongetikeane <ongetimartin24@gmail.com>
2022-09-12 08:17:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,13 @@
  *
  * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle
- * Return; 0
+ * Return: 0
  */
 int main(void)
 
 {
 	{
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle\n");
 	}
 	return (0);
 }
