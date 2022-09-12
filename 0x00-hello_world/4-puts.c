73a28b26ea664a5ae8020108b81f111dc5688cd6
ongetikeane <ongetimartin24@gmail.com>
2022-09-12 07:33:49 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,8 @@
  */
 int main(void)
 {
-    puts(**/*programming is like building a multilingual puzzle"):
-	    return (0):
+	{
+	printf("programming is like building a multilingual puzzle"):
+	}
+	return (0):
 }
