b86d17c6942e1925faa2f296f242998ad9f99460
ongetikeane <ongetimartin24@gmail.com>
2022-09-14 10:40:29 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet -> prints the lower case alphabets
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	putchar(j);
+	{
+	putchar('\n');
+}
